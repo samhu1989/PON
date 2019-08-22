904ad90f31461e123cbd853dce88f1623bc19586
Sam Hu <hsy19891228@yeah.net>
2019-08-23 04:27:52 +0800

    fix a bug in render_train_val

--- util/data/render_train_val.py
+++ util/data/render_train_val.py
@@ -14,7 +14,7 @@ def run(**kwargs):
     fs = os.listdir(val);
     for f in fs:
         if f.endswith('mesh.obj'):
-            view_path = train+os.sep+f.replace('mesh.obj','view');
+            view_path = val+os.sep+f.replace('mesh.obj','view');
             os.mkdir(view_path);
             cmd = 'blender --background --python %s -- --output_folder %s --views 16 --obj %s'%(pth+os.sep+'render_blender.py',os.path.abspath(view_path),val+os.sep+f);
             os.system(cmd);
