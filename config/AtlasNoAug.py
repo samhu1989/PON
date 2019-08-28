fb995779a02b82658e53d3ca5981174c912d310f
samhu1989 <hsy19891228@yeah.net>
2019-08-28 18:41:08 +0800

    new config

---
+++ config/AtlasNoAug.py
@@ -0,0 +1,18 @@
+#optim
+lr = 1e-5;
+optim = 'Adam'
+weight_decay = 0.0
+nepoch = 1
+#net
+pts_num = 2500
+pts_num_gt = 10000
+grid_num = 25
+grid_dim = 2
+mode = 'SVR'
+#data
+dataset = 'PON'
+category = ['Chair','StorageFurniture','Table'];
+workers = 4
+
+from .config import accuracy,loss,parameters,writelog;
+        
