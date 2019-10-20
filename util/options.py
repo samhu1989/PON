1ac162d3aea3e05492cd134c4679ae50d11865b2
Sam Hu <hsy19891228@yeah.net>
2019-10-20 17:10:24 +0800

    Update options.py

--- util/options.py
+++ util/options.py
@@ -20,6 +20,8 @@ def get_opt():
     parser.add_argument('--batch_size','-bs',type=int,default=32,help='batch size');
     parser.add_argument('--nepoch','-nepoch',type=int,help='epoch number');
     parser.add_argument('--lr_decay_freq','-lrd',type=int,default=1,help='decay lr every few epochs');
+    #int flags
+    parser.add_argument('--lr','-lr',type=float,help='learning rate');
     return parser.parse_args();
 
 def usage():
