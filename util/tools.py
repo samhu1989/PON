0c1f1d3dd1302dd5ea484a4e7e1300cad4fb911c
Sam Hu <hsy19891228@yeah.net>
2019-10-20 17:53:21 +0800

    Update tools.py

--- util/tools.py
+++ util/tools.py
@@ -38,13 +38,13 @@ class AverageValueMeter(object):
         self.reset()
 
     def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
+        self.val = 0.0
+        self.avg = 0.0
+        self.sum = 0.0
         self.count = 0.0
 
     def update(self, val, n=1):
-        self.val = val
+        self.val = float(val)
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
