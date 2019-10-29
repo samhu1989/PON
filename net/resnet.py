fdb2be0e5b7f87665f599fef83ee7dacab868ae4
Sam Hu <hsy19891228@yeah.net>
2019-10-29 08:07:03 +0800

    update network

--- net/resnet.py
+++ net/resnet.py
@@ -94,8 +94,7 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-
-    def __init__(self, block, layers,input_channel=3 ,num_classes=1000):
+    def __init__(self, block, layers,input_channel=3 ,num_classes=1000, fc=True):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(input_channel, 64, kernel_size=7, stride=2, padding=3,
@@ -107,8 +106,9 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        if fc:
+            self.avgpool = nn.AvgPool2d(7)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -145,11 +145,10 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
+        if fc:
+            x = self.avgpool(x)
+            x = x.view(x.size(0), -1)
+            x = self.fc(x)
         return x
 
 
