3483fcce9954fa3b929d1823fef95fa79ffb3cc4
Sam Hu <hsy19891228@yeah.net>
2019-11-03 14:55:16 +0800

    Update PInt.py

--- net/PInt.py
+++ net/PInt.py
@@ -6,20 +6,23 @@ def path_integral(map,path):
     
 def path_generate(start,end):
     pts = np.concatenate([start,end],axis=0);
-    pts_m = np.mean(pts,keepdim=True,axis=0);
+    pts_m = np.mean(pts,keepdims=True,axis=0);
     pts = pts - pts_m;
     u, s, vh = np.linalg.svd(pts, full_matrices=True);
-    x = np.matmul(vh,pts);
-    
-    return                                                                         
+    x = np.matmul(u,pts);
+    print(u);
+    print(vh);
+    print(x);
+    return pts_m;                                                                        
 
 def run(**kwargs):
     import matplotlib.pyplot as plt;
     start = np.random.randint(0,224,size=[1,2]);
     end = np.random.randint(0,224,size=[1,2]);
-    p = path_generate(start,end);
     img = np.ones((224,224)).astype(np.float32);
     plt.imshow(img*255.0,cmap='gray');
+    p = path_generate(start,end);
+    plt.plot(p[:,0],p[:,1],'*');
     plt.plot(start[:,0],start[:,1],'x');
     plt.plot(end[:,0],end[:,1],'o');
     plt.show();
