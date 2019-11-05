45f193ed0902344eb4dad80306f3c4ad83cd0738
Sam Hu <hsy19891228@yeah.net>
2019-11-05 15:03:02 +0800

    pathintegral

---
+++ config/VReg4.py
@@ -0,0 +1,47 @@
+pts_num = 1200;
+pts_num_gt = 1200;
+grid_num = 2;
+workers = 8;
+optim = 'Adam';
+lr = 1e-3;
+nepoch = 1;
+print_epoch = 1;
+mode = 'Reg';
+weight_decay = 0.0;
+as_text = False;
+
+from .config import parameters,NpEncoder;
+import torch;
+import numpy as np;
+
+def a2v(r,vec):
+    v = vec*np.pi;
+    th = v[:,0].contiguous();
+    phi = (v[:,1]*2.0).contiguous();
+    sth = torch.sin(th);
+    cth = torch.cos(th);
+    sphi = torch.sin(phi - np.pi);
+    cphi = torch.cos(phi - np.pi);
+    r = r.view(r.size(0));
+    x = r*sth*cphi;
+    y = r*sth*sphi;
+    z = r*cth;
+    res = torch.stack([x,y,z],dim=1);
+    return res; 
+
+def accuracy(data,out):
+    acc = {};
+    L2 = ( a2v(data[5],data[6]) - out['vec'] )**2;
+    ax = [x for x in range(1,L2.dim())];
+    acc['cd'] = torch.mean(L2,dim=ax);
+    return acc;
+    
+def loss(data,out):
+    loss = {};
+    L2 = ( a2v(data[5],data[6]) - out['vec'] )**2;
+    ax = [x for x in range(1,L2.dim())];
+    loss['cd'] = torch.mean(L2,dim=ax);
+    loss['overall'] = torch.mean(loss['cd']);
+    return loss;
+    
+from .VReg1 import writelog;
--- net/PInt.py
+++ net/PInt.py
@@ -1,37 +1,74 @@
 import torch;
 import numpy as np;
-
-def path_integral(map,path):
-    return 
-    
-def path_generate(start,end):
-    pts = np.concatenate([start,end],axis=0);
-    pts_m = np.mean(pts,keepdims=True,axis=0);
-    pts = pts - pts_m;
-    u, s, vh = np.linalg.svd(pts, full_matrices=True);
-    xend = np.matmul(vh,pts.T).T;
-    num = int(np.abs(xend[1,0]-xend[0,0]));
-    x = np.zeros([num,2],dtype=np.float32);
-    for i,xtmp in enumerate(np.linspace(xend[0,0],xend[1,0],num)):
-        x[i,0] = xtmp;
-        x[i,1] = 0.008*(xtmp - xend[0,0])*(xtmp - xend[1,0]) + (xend[0,1]+xend[1,1])/2.0;
-    p = np.matmul(vh.T,x.T).T;
-    p += pts_m;
-    return p;
-
-def path_bspline(start,end):
+import torch.nn as nn;
+from .unet import UNet;
+# map (b,6,224,224)
+# p (b,100)
+# dp(b,2,100)
+def path_integral(map,p,dp):
+    map = map.view(map.size(0),6,-1);#(b,6,224x224)
+    p = p.unsqueeze(1).repeat(1,6,1).contiguous();#(b,6,num)
+    v = torch.gather(map,2,p.long());#(b,6,num)
+    v = v.view(v.size(0),2,3,-1);
+    dp = dp.view(dp.size(0),2,1,-1);
+    dp = dp.type(v.type());
+    return torch.sum(v*dp,dim=[1,3]);
+#
+def path_generate(s,e,h,w,num=200):
+    return line(s,e,h,w,num);
+#    
+def line(s,e,h,w,num):
+    b = s.size(0);
+    t = torch.linspace(0.0,1.0,num);
+    t = t.view(1,num);
+    t = t.type(s.type());
+    x = t*s[:,:1] + (1.0-t)*e[:,:1];
+    y = t*s[:,1:] + (1.0-t)*e[:,1:];
+    p = y*w + x;
+    dp = ( e - s ) / float(num);
+    dp = dp.view( dp.size(0), dp.size(1), 1 );
+    dp = dp.repeat(1,1,num);
+    return p,dp;
+#
+def bspline(start,end):
     return;
-
+#    
+class Net(nn.Module):
+    def __init__(self,**kwargs):
+        super(Net,self).__init__();
+        self.unet = UNet(3,6);
+        
+    def forward(self,input):
+        img = input[0];
+        s = input[7];
+        e = input[8];
+        x = img[:,:,:,:3].contiguous();
+        x = x.permute(0,3,1,2).contiguous();
+        dmap = self.unet(x);
+        p,dp = path_generate(s,e,img.size(2),img.size(3));
+        y = path_integral(dmap,p,dp);
+        out = {'vec':y};
+        out['y'] = self.v2a(y);
+        return out;
+        
+    def v2a(self,vec):
+        r = torch.sqrt(torch.sum((vec[:,:3].contiguous())**2,dim=1));
+        theta = torch.acos(vec[:,2].contiguous()/r) / np.pi;
+        phi = ( torch.atan2(vec[:,1].contiguous(),vec[:,0].contiguous()) + np.pi )/2.0/np.pi;
+        rt = torch.stack([theta.view(-1),phi.view(-1)],dim=1);
+        return rt;
+        
 def run(**kwargs):
     import matplotlib.pyplot as plt;
-    start = np.random.randint(0,224,size=[1,2]);
-    end = np.random.randint(0,224,size=[1,2]);
-    img = np.ones((224,224)).astype(np.float32);
-    p = path_generate(start,end);
-    plt.subplot(111);
-    plt.imshow(img*255.0,cmap='gray');
-    plt.plot(p[:,0],p[:,1],'*');
-    plt.plot(start[:,0],start[:,1],'x');
-    plt.plot(end[:,0],end[:,1],'o');
-    plt.show();
+    from PIL import Image;
+    im = Image.open("./data/exp.png",'r');
+    pim = np.array(im);
+    im = torch.from_numpy(pim);
+    print(im.size());
+    im = im.permute(2,0,1).contiguous();
+    print(im.size());
+    x = im[0,:,:].numpy();
+    print(x.shape);
+    im = Image.fromarray(x,'L');
+    im.save("./log/out.png");
     return;
