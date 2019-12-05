b945884bfcb1bcd0cb7019be05656987f97d7aeb
Sam Hu <hsy19891228@yeah.net>
2019-12-05 13:04:21 +0800

    Update BB10VAE.py

--- config/BB10VAE.py
+++ config/BB10VAE.py
@@ -7,30 +7,7 @@ from datetime import datetime;
 import json;
 import numpy as np;
 from .config import NpEncoder;
-from .BB1VAE import writelog;
-beta = 10;
-input_size = 24;
-latent_size = 1024;
-z_size = 16;
-workers = 4;
-lr = 1e-3;
-weight_decay = 0.0;
-nepoch = 1000;
-category = ['Chair','Table','StorageFurniture','Bed'];
+from .BB1VAE import writelog,input_size,latent_size,z_size,workers,lr,weight_decay,nepoch,category,part_idx,loss,parameters;
 
-def loss(data,out):
-    x = data[0];
-    recon_x = out['rx'];
-    mu = out['mu'];
-    logvar = out['logvar'];
-    loss = {};
-    loss['kl'] =  ( -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) ) / x.size(0);
-    loss['recon'] = torch.sum( ( recon_x - x )**2 ) / x.size(0) ;
-    loss['betakl'] = beta*loss['kl'];
-    loss['overall'] = loss['betakl'] + loss['recon'];
-    return loss;
-    
-    
-def parameters(net):
-    return net.parameters(); # train all parameters
+beta = 10;
     
