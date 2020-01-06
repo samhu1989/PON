4bff53df68a8d4b131870685fd9782517a3c88a2
Sam Hu <hsy19891228@yeah.net>
2020-01-06 14:42:56 -0800

    predict touch only

---
+++ config/Touch.py
@@ -0,0 +1,101 @@
+import os;
+import sys;
+import torch;
+import pandas as pd;
+from util.tools import write_tfb;
+from datetime import datetime;
+import json;
+import numpy as np;
+from .config import NpEncoder;
+import torch.nn as nn;
+
+workers = 4;
+lr = 1e-3;
+weight_decay = 0.0;
+nepoch = 1000;
+print_epoch = 1;
+
+def loss(data,out):
+    ygt = data[3];
+    vgt = data[4];
+    yout = out['y'];
+    vout = out['vec'];
+    loss = {};
+    bce = nn.BCELoss(reduction='none')
+    loss['bce'] = bce(yout,ygt.data);
+    loss['acc'] = (( yout.data > 0.5) == ygt.data ); 
+    loss['overall'] = torch.mean( loss['bce'] );
+    return loss;
+    
+def accuracy(data,out):
+    ygt = data[3];
+    vgt = data[4];
+    yout = out['y'];
+    vout = out['vec'];
+    loss = {};
+    bce = nn.BCELoss(reduction='none')
+    loss['bce'] = bce(yout,ygt.data);
+    loss['acc'] = (( yout.data > 0.5) == ygt.data ); 
+    loss['overall'] = loss['bce'];
+    return loss;
+    
+def parameters(net):
+    return net.parameters(); # train all parameters
+    
+bestcnt = 1;
+best = np.array([10000]*bestcnt,dtype=np.float32);
+bestn = [""]*bestcnt;
+
+def writelog(**kwargs):
+    global best;
+    global bestn;
+    opt = kwargs['opt'];
+    iepoch = kwargs['iepoch'];
+    nepoch = opt['nepoch'];
+    ib = kwargs['idata'];
+    nb = kwargs['ndata'] // opt['batch_size'];
+    out = kwargs['out'];
+    net = kwargs['net'];
+    d = kwargs['data'];
+    meter = kwargs['meter'];
+    optim = kwargs['optim'];
+    print('['+str(datetime.now())+'][%d/%d,%d/%d]'%(iepoch,nepoch,ib,nb)+'training:'+str(kwargs['istraining']));
+    if not 'log_tmp' in opt.keys():
+        opt['log_tmp'] = opt['log']+os.sep+opt['net']+'_'+opt['config']+'_'+opt['mode']+'_'+str(datetime.now()).replace(' ','-').replace(':','-');
+        os.mkdir(opt['log_tmp']);
+        with open(opt['log_tmp']+os.sep+'options.json','w') as f:
+            json.dump(opt,f,cls=NpEncoder);
+        nparam = 0;
+        with open(opt['log_tmp']+os.sep+'net.txt','w') as logtxt:
+            print(str(kwargs['net']),file=logtxt);
+            for p in parameters(kwargs['net']):
+                nparam += torch.numel(p);
+            print('nparam:%d'%nparam,file=logtxt);
+        
+    with open(opt['log_tmp']+os.sep+'log.txt','a') as logtxt:
+        print('['+str(datetime.now())+'][%d/%d,%d/%d]'%(iepoch,nepoch,ib,nb)+'training:'+str(kwargs['istraining']),file=logtxt);
+        print(json.dumps(meter,cls=NpEncoder),file=logtxt);
+    if kwargs['istraining']:
+        tfb_dir = os.path.join(opt['log_tmp'],'tfb','train');
+    else:
+        tfb_dir = os.path.join(opt['log_tmp'],'tfb','test');
+    write_tfb(tfb_dir,meter,ib+nb*iepoch,nb,optim);
+    
+    if not kwargs['istraining'] and ib >= nb-1:
+        if meter['overall'].overall_meter.avg < best[-1]:
+            fn = bestn[-1];
+            if fn:
+                os.remove(opt['log_tmp']+os.sep+fn);
+            fn = 'net_'+str(datetime.now()).replace(' ','-').replace(':','-')+'.pth';
+            best[-1] = meter['overall'].overall_meter.avg;
+            bestn[-1] = fn;
+            torch.save(net.state_dict(),opt['log_tmp']+os.sep+fn);
+            idx = np.argsort(best);
+            best = best[idx];
+            bestn = [bestn[x] for x in idx.tolist()];
+            bestdict = dict(zip(bestn, best.tolist()));
+            print(best);
+            print(bestn);
+            print(bestdict);
+            with open(opt['log_tmp']+os.sep+'best.json','w') as f:
+                json.dump(bestdict,f);
---
+++ net/Touch.py
@@ -0,0 +1,67 @@
+import torch;
+import torch.nn as nn;
+import torch.nn.functional as F;
+import net.resnet as resnet;
+import numpy as np;
+        
+class TouchNet(nn.Module):
+    def __init__(self,**kwargs):
+        super(TouchNet,self).__init__();
+        self.enc = resnet.resnet18(pretrained=False,input_channel=4,fc=False);
+        self.dec = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        );
+        self.avgpool = nn.AvgPool2d(7);
+        self.fc = nn.Linear(512,1);
+                
+    def forward(self,x1,x2):
+        #
+        x1 = self.enc(x1);
+        x2 = self.enc(x2);
+        x = torch.cat([x1,x2],axis=1);
+        x = self.dec(x);
+        #
+        x = self.avgpool(x);
+        x = x.view(x.size(0), -1);
+        x = self.fc(x);
+        y = F.sigmoid(x);
+        return y;
+
+class Net(nn.Module):
+    def __init__(self,**kwargs):
+        super(Net,self).__init__();
+        self.mode = kwargs['mode'];
+        self.cnet = CNet();
+        
+    def forward(self,input):
+        img = input[0];
+        x = img[:,:,:,:3].contiguous();
+        x = x.permute(0,3,1,2).contiguous();
+        #
+        ms = input[1].unsqueeze(1);
+        mt = input[2].unsqueeze(1);
+        #
+        x1,x2 = self.add_msk(x,ms,mt);
+        y = self.cnet(x1,x2);
+        #
+        out = {'y':y};
+        return out;
+        
+    def add_msk(self,x,ms,mt):
+        if self.mode == 'full':
+            x1 = torch.cat([x,ms],axis=1);
+            x2 = torch.cat([x,mt],axis=1);
+        elif self.mode == 'part':
+            part = x*(ms+mt);
+            x1 = torch.cat([part,ms],axis=1);
+            x2 = torch.cat([part,mt],axis=1);
+        else:
+            assert False, "Unkown mode";
+        return x1,x2;
+            
+        
+        
+        
+        
