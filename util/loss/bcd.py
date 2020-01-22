26f0938d1447d2e5519abbb40a7fb282f83c7ae3
Sam Hu <hsy19891228@yeah.net>
2020-01-22 16:24:09 +0800

    using pip install chamferdist

--- util/loss/bcd.py
+++ util/loss/bcd.py
@@ -2,12 +2,11 @@ import torch;
 import sys;
 import numpy as np;
 from net.cageutil import rot9,sr2box;
+import pandas as pd;
 from util.data.ply import write_ply, read_ply;
-sys.path.append('./ext/');
-sys.path.append('./ext/cd/install/Lib/site-packages');
-import cd.dist_chamfer_idx as ext;
 from util.data.gen_toybox import box_face;
-distChamfer =  ext.chamferDist();
+from chamferdist import ChamferDistance;
+distChamfer =  ChamferDistance();
 
 bcd_pts = np.zeros([1200,3],dtype=np.float32);
 span = np.random.uniform(-1.0,1.0,[200,2]);
@@ -47,22 +46,27 @@ def box_cd(osize,or1,or2,gtsize,gtr1,gtr2):
     gtpts = sr2pts(gtsize,gtr1,gtr2);
     dist1, dist2, idx1, idx2 = distChamfer(opts,gtpts);
     w = size2w(gtsize);
-    w1 = torch.gather(w,idx1,dim=1);
+    w1 = torch.gather(w,1,idx1.long());
     ax1 = [x for x in range(1,dist1.dim())];
     ax2 = [x for x in range(1,dist2.dim())];
     cd_loss = torch.mean(dist1*w1,dim=ax1)+torch.mean(dist2*w,dim=ax2);
     return cd_loss;
     
 def write_box(prefix,box):
+    T=np.dtype([("n",np.uint8),("i0",np.int32),('i1',np.int32),('i2',np.int32)]);
+    fidx = box_face;
+    face = np.zeros(shape=[fidx.shape[0]],dtype=T);
+    for i in range(fidx.shape[0]):
+        face[i] = (3,fidx[i,0],fidx[i,1],fidx[i,2]);
     for i in range(box.shape[0]):
-        write_ply(prefix+'%d.ply'%i,points=pd.DataFrame(box[i,...]),faces=pd.DataFrame(face));
+        write_ply(prefix+'_%d.ply'%i,points=pd.DataFrame(box[i,...]),faces=pd.DataFrame(face));
 
 def run(**kwargs):
     nepoch = kwargs['nepoch'];
     bs = kwargs['batch_size'];
-    size_gt = np.random.uniform(0.1,1,[bs,3]);
-    r1_gt = np.random.uniform(-1,1,[bs,3]);
-    r2_gt = np.random.uniform(-1,1,[bs,3]);
+    size_gt = torch.from_numpy(np.random.uniform(0.1,1,[bs,3]).astype(np.float32));
+    r1_gt = torch.from_numpy(np.random.uniform(-1,1,[bs,3]).astype(np.float32));
+    r2_gt = torch.from_numpy(np.random.uniform(-1,1,[bs,3]).astype(np.float32));
     size = torch.zeros([bs,3],requires_grad=True);
     r1 = torch.zeros([bs,3],requires_grad=True);
     r2 = torch.zeros([bs,3],requires_grad=True);
@@ -70,14 +74,16 @@ def run(**kwargs):
     r1.data.normal_(0.0,1.0);
     r2.data.normal_(0.0,1.0);
     opt = torch.optim.Adam([size,r1,r2],lr=0.001);
-    boxgt = sr2box(sizegt,r1_gt,r2_gt);
+    boxgt = sr2box(size_gt.cuda(),r1_gt.cuda(),r2_gt.cuda());
     write_box('./log/boxcd/gt',boxgt.data.cpu().numpy());
     for i in range(nepoch):
         opt.zero_grad();
-        cd = box_cd(size,r1,r2,size_gt,r1_gt,r2_gt);
+        cd = box_cd(size.cuda(),r1.cuda(),r2.cuda(),size_gt.cuda(),r1_gt.cuda(),r2_gt.cuda());
         loss = torch.mean(cd);
         loss.backward();
+        print(loss.data.cpu().numpy());
         opt.step();
-        boxout = sr2box(size,r1,r2);
-        write_box('./log/boxcd/out_%d'%i,boxout.data.cpu().numpy());
+        boxout = sr2box(size.cuda(),r1.cuda(),r2.cuda());
+        if i%100 == 0:
+            write_box('./log/boxcd/out_%d'%i,boxout.data.cpu().numpy());
     return;
