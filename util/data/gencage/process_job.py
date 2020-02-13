82c4789a3f21c31d62d3b5adb61feee1e150efb1
Sam Hu <hsy19891228@yeah.net>
2020-02-14 03:13:11 +0800

    Update process_job.py

--- util/data/gencage/process_job.py
+++ util/data/gencage/process_job.py
@@ -5,11 +5,22 @@ import sys;
 from scipy.spatial.transform import Rotation as R;
 import usesstk;
 import useblender;
+import OpenEXR, Imath, cv;
 
 dataroot = '/cephfs/siyu/cage';
 pndata = 'partnet.zip';
 spndata = 'shapenet.zip';
 tmproot = '/cephfs/siyu/cage/tmp';
+
+def auto_canny(image, sigma=0.33):
+	# compute the median of the single channel pixel intensities
+	v = np.median(image)
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv.Canny(image, lower, upper)
+	# return the edged image
+	return edged
 
 def do_one(job):
     id = job.split('/')[2];
@@ -27,8 +38,49 @@ def render_obj(objpath):
 def render_depth_normal(objpath):
     path = os.path.dirname(objpath);
     for f in os.listdir(path):
-        if 'model_normalized_r' in f:
+        if 'model_normalized_r' in f and f.endswith('.obj'):
             useblender.render(os.path.join(path,f));
+            
+def add_edge(objpath):
+    rgb = objpath.replace('.obj','.png');
+    depth = objpath.replace('.obj','_depth.exr0001.exr');
+    norm = objpath.replace('.obj','_normal.exr0001.exr');
+    rgbo = objpath.replace('.obj','_e.png');
+    #get depth img
+    pt = Imath.PixelType(Imath.PixelType.FLOAT)
+    dimg = OpenEXR.InputFile(depth);
+    dw = dimg.header()['dataWindow'];
+    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1);   
+    ddata = golden.channel('Z',pt)
+    dimg = cv.CreateMat(size[1], size[0], cv.CV_32FC1)
+    cv.SetData(dimg,ddata);
+    normdimg = np.zeros((size[1], size[0]))
+    cv.normalize(dimg,normdimg,0,255,cv.NORM_MINMAX);
+    dedge = auto_canny(normdimg.astype(np.uint8));
+    #get normal img
+    pt = Imath.PixelType(Imath.PixelType.FLOAT)
+    nimg = OpenEXR.InputFile(norm);
+    rCh = nimg.extract_channels();
+    ndata = np.asarray(rCh);
+    normnimg = np.zeros((size[1], size[0]));
+    cv.normalize(np.mean(dimg,axis=2),normnimg,0,255,cv.NORM_MINMAX);
+    nedge = auto_canny(normnimg.astype(np.uint8));
+    #
+    edge = np.bitwise_or(dedge,nedge);
+    rgbimg = cv.imread(rgb);
+    out = rgbimg.copy();
+    out[edge>0,0] = 0.0;
+    out[edge>0,1] = 0.0;
+    out[edge>0,2] = 0.0;
+    cv2.imwrite(rgbo,out);
+
+            
+def highlight_edge(objpath):
+    path = os.path.dirname(objpath);
+    for f in os.listdir(path):
+        if 'model_normalized_r' in f and f.endswith('.obj'):
+            add_edge(os.path.join(path,f));
+    
             
 def align_obj_with_N_random_rot(inobj,N=4):
     pv = [];
