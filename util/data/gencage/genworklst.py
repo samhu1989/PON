2dc0d514c2a674520715687b9a2743978ca51177
Sam Hu <hsy19891228@yeah.net>
2020-02-13 14:51:39 +0800

    Update genworklst.py

--- util/data/gencage/genworklst.py
+++ util/data/gencage/genworklst.py
@@ -6,13 +6,14 @@ dataroot = '/cephfs/siyu/cage';
 pndata = 'partnet.zip';
 spndata = 'shapenet.zip';
 caseperjob = 80;
-
+cat_lst = [];
 if __name__ == '__main__':
     with zpf.ZipFile(os.path.join(dataroot,spndata),'r') as spnzip:
         casecnt = 0;
         jobcnt = 0;
         current_lst = None;
         newlst = True;
+        debug_lst = open(os.path.join(dataroot,'job_debug.txt'),'w');
         for info in spnzip.infolist():
             if casecnt == 0 and newlst:
                 if current_lst is not None:
@@ -28,5 +29,9 @@ if __name__ == '__main__':
                 if casecnt >= caseperjob:
                     casecnt = 0;
                     newlst = True;
+                cat = info.filename.split(os.sep)[1];
+                if not cat in cat_lst:
+                    cat_lst.append(cat);
+                    print(info.filename,file = debug_lst);
                
     
