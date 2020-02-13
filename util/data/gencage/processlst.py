76cf2b355b0fc39603a9aa365eafee5b9aa9b36b
Sam Hu <hsy19891228@yeah.net>
2020-02-13 15:15:38 +0800

    Create processlst.py

---
+++ util/data/gencage/processlst.py
@@ -0,0 +1,20 @@
+import zipfile as zpf;
+import os;
+import numpy as np;
+import sys;
+dataroot = '/cephfs/siyu/cage';
+pndata = 'partnet.zip';
+spndata = 'shapenet.zip';
+
+def extract_job_data(job):
+    print(job);
+
+    
+
+if __name__ == '__main__':
+    lst = sys.args[1];
+    with open(os.path.join(,) ,'r') as joblst:
+        for job in joblst:
+            extract_job_data(job);
+            exit();
+            
