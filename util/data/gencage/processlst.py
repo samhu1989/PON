98c90a6f529307c290e15928e035fa7e877910ca
Sam Hu <hsy19891228@yeah.net>
2020-02-13 15:36:41 +0800

    Update processlst.py

--- util/data/gencage/processlst.py
+++ util/data/gencage/processlst.py
@@ -2,15 +2,21 @@ import zipfile as zpf;
 import os;
 import numpy as np;
 import sys;
+
 dataroot = '/cephfs/siyu/cage';
 pndata = 'partnet.zip';
 spndata = 'shapenet.zip';
 
 def extract_job_data(job):
-    print(job);
-
+    id = job.split('/')[2];
+    #create tmp workspace
+    workpath = os.path.join(dataroot,'tmp',id);
+    os.makedirs(os.path.join(dataroot,'tmp',id));
+    with zpf.ZipFile(os.path.join(dataroot,spndata),'r') as spnzip:
+        spnzip.extract(job,path=workpath);
+    with zpf.ZipFile(os.path.join(dataroot,pndata),'r') as pnzip:
+        pnzip.extract('partnet/'+id,path=workpath);
     
-
 if __name__ == '__main__':
     lst = sys.argv[1];
     with open(os.path.join(dataroot,lst) ,'r') as joblst:
