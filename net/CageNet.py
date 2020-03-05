bfbe9e0f52b425c9a511fd40acd66e29b0a49d13
Sam Hu <hsy19891228@yeah.net>
2020-03-05 04:17:28 -0800

    redesign CageNet all together

--- net/CageNet.py
+++ net/CageNet.py
@@ -4,66 +4,61 @@ import torch.nn.functional as F;
 import net.resnet as resnet;
 import numpy as np;
 
-class BoxNet(nn.Module):
+class ImEnc(nn.Module):
+    def __init__(self,**kwargs):
+        super(ImEnc,self).__init__();
+        #part instance encoder
+        self.nenc = resnet.resnet18(pretrained=False,input_channel=4,fc=False);
+        #part pair encoder
+        self.penc = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+            nn.Conv2d(512, 512, kernel_size=1, bias=False),
+            nn.BatchNorm2d(512)
+        );
+        
+    def forward(self,x,pi,pj):
+        nf = self.nenc(x);
+        pnfi = torch.index_select(nf,dim=0,pi);
+        pnfj = torch.index_select(nf,dim=0,pj);
+        pnf = torch.stack([pnfi,pnfj],dim=1);
+        pf = self.penc(pnf);
+        return nf,pf;
+
+class BoxDec(nn.Module):
     def __init__(self,**kwargs):
         super(BoxNet,self).__init__();
-        self.enc = resnet.resnet18(pretrained=False,input_channel=3,num_classes=512);
         self.dec_size = nn.Sequential(
-                nn.Linear(512,512),
+                nn.Conv2d(512, 256, kernel_size=1, bias=False),
+                nn.BatchNorm2d(256),
                 nn.ReLU(inplace=True),
-                nn.Linear(512,3)
-                );
-        self.dec_r1 = nn.Sequential(
-                nn.Linear(512,512),
+                nn.AvgPool2d(7),
+                nn.Conv2d(256, 256, kernel_size=1, bias=True),
                 nn.ReLU(inplace=True),
-                nn.Linear(512,3)
-                );
-        self.dec_r2 = nn.Sequential(
-                nn.Linear(512,512),
-                nn.ReLU(inplace=True),
-                nn.Linear(512,3)
-                );
-                
-    def forward(self,img):
-        y = self.enc(img);
-        size = self.dec_size(y);
-        size = torch.max(size+0.5,torch.sigmoid(size));
-        r1 = self.dec_r1(y);
-        r2 = self.dec_r2(y);
-        return size,r1,r2;
-        
-class CNet(nn.Module):
-    def __init__(self,**kwargs):
-        super(CNet,self).__init__();
-        self.enc = resnet.resnet18(pretrained=False,input_channel=5,num_classes=512);
-        self.dec_is = nn.Sequential(
-                nn.Linear(512,512),
-                nn.ReLU(inplace=True),
-                nn.Linear(512,1),
+                nn.Conv2d(256, 3, kernel_size=1, bias=True),
                 nn.Sigmoid()
                 );
-        self.dec_x1 = nn.Sequential(
-                nn.Conv1d(512+3,64,1),
+        self.dec_rot6 = nn.Sequential(
+                nn.Conv2d(512, 256, kernel_size=1, bias=False),
+                nn.BatchNorm2d(256),
                 nn.ReLU(inplace=True),
-                nn.Conv1d(64,1,1)
-                );
-        self.dec_x2 = nn.Sequential(
-                nn.Conv1d(512+3,64,1),
+                nn.AvgPool2d(7),
+                nn.Conv2d(256, 256, kernel_size=1, bias=True),
                 nn.ReLU(inplace=True),
-                nn.Conv1d(64,1,1)
+                nn.Conv2d(256, 6, kernel_size=1, bias=True),
+                nn.Tanh()
                 );
                 
-    def forward(self,img,c1,c2):
-        x = self.enc(img);
-        y = self.dec_is(x);
-        f = x.unsqueeze(2).repeat(1,1,c1.size(2)).contiguous();
-        expf1 = torch.cat((c1,f),1).contiguous();
-        expf2 = torch.cat((c2,f),1).contiguous();
-        w1 = self.dec_x1(expf1);
-        w1 = F.softmax(w1,dim=2);
-        w2 = self.dec_x2(expf2);
-        w2 = F.softmax(w2,dim=2);
-        return y,w1,w2;
+    def forward(self,x):
+        size = self.dec_size(x);
+        size = size.view(size.size(0),-1);
+        rot6 = self.dec_rot6(x);
+        rot6 = rot6.view(rot6.size(0),6);
+        r1 = rot6[:,0:3].contiguous();
+        r2 = rot6[:,3:6].contiguous();
+        return size,r1,r2;
+    
 
 class Net(nn.Module):
     def __init__(self,**kwargs):
@@ -72,60 +67,7 @@ class Net(nn.Module):
         self.bnet = BoxNet();
         
     def forward(self,input):
-        img = input[0];
-        x = img[:,:,:,:3].contiguous();
-        x = x.permute(0,3,1,2).contiguous();
-        #
-        ms = input[1].unsqueeze(1);
-        mt = input[2].unsqueeze(1);
-        #
-        xms = x*ms;
-        xmt = x*mt;
-        const = np.array([[[1,1,-1],[-1,1,-1],[-1,1,1],[1,1,1],[1,-1,-1],[-1,-1,-1],[-1,-1,1],[1,-1,1]]],dtype=np.float32);
-        const = torch.from_numpy(const);
-        const = const.type(x.type());
-        #
-        ss,sr1,sr2 = self.bnet(xms);
-        with torch.no_grad():
-            srot = self.rot(sr1,sr2);
-        c1 = torch.matmul(const,srot);
-        c1 = c1.permute(0,2,1).contiguous();
-        #
-        ts,tr1,tr2 = self.bnet(xmt);
-        with torch.no_grad():
-            trot = self.rot(tr1,tr2);
-        c2 = torch.matmul(const,trot);
-        c2 = c2.permute(0,2,1).contiguous();
-        #
-        xmst = x*(ms+mt);
-        xmst_ms_mt = torch.cat([xmst,ms,mt],dim=1);
-        y,ws,wt = self.cnet(xmst_ms_mt,c1,c2);
-        #
-        with torch.no_grad():
-            coords = torch.matmul(const*ss.unsqueeze(1).contiguous(),srot);
-            coords = coords.permute(0,2,1).contiguous();
-            coordt = torch.matmul(const*ts.unsqueeze(1).contiguous(),trot);
-            coordt = coordt.permute(0,2,1).contiguous();
-        #
-        coords = torch.sum(ws*coords,dim=2);
-        coordt = torch.sum(wt*coordt,dim=2);
-        vec = torch.cat([ss,sr1,sr2,ts,coords-coordt,tr1,tr2],dim=1);
-        out = {'xms':xms,'xmt':xmt,'xmst':xmst,'y':y,'vec':vec,'xs':coords,'xt':coordt};
-        return out;
-        
-    def rot(self,r1,r2):
-        if self.training:
-            r3 = torch.cross(r1,r2);
-            rot = torch.stack([r1,r2,r3],dim=1);
-            rot = rot.view(-1,3,3);
-        else:
-            rr1 = r1 / torch.sqrt(torch.sum(r1**2,dim=1,keepdims=True));
-            rr2 = r2 - torch.sum(r2*rr1,dim=1,keepdims=True)*rr1;
-            rr2 = rr2 / torch.sqrt(torch.sum(rr2**2,dim=1,keepdims=True));
-            r3 = torch.cross(rr1,rr2);
-            rot = torch.stack([rr1,rr2,r3],dim=1);
-            rot = rot.view(-1,3,3);
-        return rot;
+
         
         
         
